250a30fe7c58657ff36bcb981e022647fad31e92
M0rky123 <smutny.vojta@gmail.com>
2023-04-22 00:11:40 +0200

    audio-predone

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ const songs = [
   { song: "Closer", author: "The Chainsmokers", img: "./songs/edm/closer.jpg", audio: "./songs/edm/closer.mp3" },
   { song: "Levels", author: "Avicii", img: "./songs/edm/levels.jpg", audio: "./songs/edm/levels.mp3" },
   { song: "Silence", author: "Marshmello ft. Khalid", img: "./songs/edm/silence.jpg", audio: "./songs/edm/silence.mp3" },
-  { song: "Friends In Low Places", author: "Garth Brooks", img: "./songs/hiphop/friendsinlowplaces.jpg", audio: "./songs/hiphop/.mp3" },
+  { song: "Friends In Low Places", author: "Garth Brooks", img: "./songs/hiphop/friendsinlowplaces.jpg", audio: "./songs/hiphop/friendsinlowplaces.mp3" },
   { song: "I Will Always Love You", author: "Dolly Patron", img: "./songs/hiphop/iwillalwaysloveyou.jpg", audio: "./songs/hiphop/iwillalwaysloveyou.mp3" },
   { song: "The Gambler", author: "Kenny Rogers", img: "./songs/hiphop/thegambler.jpg", audio: "./songs/hiphop/thegambler.mp3" },
-  { song: "Sing, Sing, Sing", author: "Benny Goodman", img: "./songs/hiphop/singsingsing.jpg", audio: "./songs/hiphop/singsingsing.mp3" },
-  { song: "Take Five", author: "Dave Brubeck", img: "./songs/hiphop/takefive.jpg", audio: "./songs/hiphop/takefive.mp3" },
-  { song: "Take the 'A' Train", author: "Duke Ellington", img: "./songs/hiphop/.jpg", audio: "./songs/hiphop/.mp3" },
-  { song: "Bag Guy", author: "Billie Eilish", img: "./songs/hiphop/.jpg", audio: "./songs/hiphop/badguy.mp3" },
-  { song: "Billie Jeans", author: "Michael Jackson", img: "./songs/hiphop/.jpg", audio: "./songs/hiphop/billiejeans.mp3" },
-  { song: "Shape Of You", author: "Ed Sheeran", img: "./songs/hiphop/.jpg", audio: "./songs/hiphop/shapeofyou.mp3" },
+  { song: "Sing, Sing, Sing", author: "Benny Goodman", img: "./songs/jazz/singsingsing.jpg", audio: "./songs/jazz/singsingsing.mp3" },
+  { song: "Take Five", author: "Dave Brubeck", img: "./songs/jazz/takefive.jpg", audio: "./songs/jazz/takefive.mp3" },
+  { song: "Take the 'A' Train", author: "Duke Ellington", img: "./songs/jazz/taketheatrain.jpg", audio: "./songs/jazz/taketheatrain.mp3" },
+  { song: "Bag Guy", author: "Billie Eilish", img: "./songs/pop/badguy.jpg", audio: "./songs/pop/badguy.mp3" },
+  { song: "Billie Jeans", author: "Michael Jackson", img: "./songs/pop/billiejeans.jpg", audio: "./songs/pop/billiejeans.mp3" },
+  { song: "Shape Of You", author: "Ed Sheeran", img: "./songs/pop/shapeofyou.jpg", audio: "./songs/pop/shapeofyou.mp3" },
   { song: "My Girl", author: "The Temptations", img: "./songs/r&b/mygirl.jpg", audio: "./songs/r&b/mygirl.mp3" },
   { song: "Recpect", author: "Aretha Franklin", img: "./songs/r&b/respect.jpg", audio: "./songs/r&b/respect.mp3" },
   {
@@ -40,21 +40,46 @@ const songs = [
 ];
 
 // TOP 10 RANDOMIZER
-
-let topSongsArray = songs;
-let randomNum = Math.ceil(Math.random() * 27);
 let topTenCards = [
-  document.querySelector(".card1"),
-  document.querySelector(".card2"),
-  document.querySelector(".card3"),
-  document.querySelector(".card4"),
-  document.querySelector(".card5"),
-  document.querySelector(".card6"),
-  document.querySelector(".card7"),
-  document.querySelector(".card8"),
-  document.querySelector(".card9"),
-  document.querySelector(".card10"),
+  document.querySelector(".song1"),
+  document.querySelector(".song2"),
+  document.querySelector(".song3"),
+  document.querySelector(".song4"),
+  document.querySelector(".song5"),
+  document.querySelector(".song6"),
+  document.querySelector(".song7"),
+  document.querySelector(".song8"),
+  document.querySelector(".song9"),
+  document.querySelector(".song10"),
 ];
+
+window.addEventListener("load", topTenSongs());
+
+function topTenSongs() {
+  let arraycache = shuffle(songs);
+  for (let i = 0; i < topTenCards.length; i++) {
+    topTenCards[i].children[0].children[0].src = arraycache[i].img;
+    topTenCards[i].children[1].children[0].textContent = arraycache[i].song;
+    topTenCards[i].children[1].children[1].textContent = arraycache[i].author;
+    if (topTenCards[i].children[1].children[0].textContent.length > 22) {
+      topTenCards[i].children[1].children[0].classList.add("balance");
+    }
+  }
+}
+
+// Vypůjčeno ze stackoverflow :^)
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 // VOLUME CONTROL
 let volume = document.getElementById("volume");
@@ -62,14 +87,36 @@ let volumeImg = document.getElementById("volumeImg");
 const page = window.location.pathname.split("/").pop();
 
 volume.addEventListener("input", () => {
-  console.log(volume.value);
   if (volume.value >= 66) {
     volumeImg.src = "./imgs/svg/volume-high.svg";
-  } else if (volume.value >= 33 && volume.value < 66) {
+  } else if (volume.value >= 33) {
     volumeImg.src = "./imgs/svg/volume-medium.svg";
-  } else if (volume.value >= 1 && volume.value < 33) {
+  } else if (volume.value >= 1) {
     volumeImg.src = "./imgs/svg/volume-low.svg";
   } else {
     volumeImg.src = "./imgs/svg/volume-off.svg";
   }
 });
+
+volume.addEventListener("input", function () {
+  audio.volume = volume.value / 100;
+});
+
+volumeImg.addEventListener("click", () => {});
+
+// PLAYER ITSELF
+const back = document.getElementById("back");
+const play = document.getElementById("play");
+const next = document.getElementById("next");
+let timestamp = document.getElementById("timestamp");
+let song;
+
+back.addEventListener("click", () => {});
+play.addEventListener("click", () => {});
+next.addEventListener("click", () => {});
+
+timestamp.addEventListener("click", () => {
+  song.currentTime = timestamp.value;
+});
+
+play.addEventListener("click", () => {});
