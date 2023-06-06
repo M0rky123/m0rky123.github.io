70c32c55d7913a63a56993b03801eab5daf028b9
M0rky123 <smutny.vojta@gmail.com>
2023-06-06 19:31:55 +0200

    ASIDE - DONE

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 import { database } from "./database.js";
 
 console.log(database);
+
+const songTitleContainer = document.querySelector(".song");
+const songTitle = document.querySelector(".song > :first-child");
+if (songTitle.clientWidth > songTitleContainer.clientWidth) {
+  songTitle.classList.toggle("titleLonger");
+}
+
+console.log(songTitle);
+console.log(songTitleContainer);
